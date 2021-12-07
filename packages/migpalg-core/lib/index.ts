e22813828076e837dce04fefbc46b09abc373f43
Miguel González <migpalg@gmail.com>
2021-12-07 16:14:31 -0500

    feat: first commit
    This commit contains some tooling to manage monorepo and build types for
    typescript libraries

---
+++ packages/migpalg-core/lib/index.ts
@@ -0,0 +1,8 @@
+/**
+ * greets a person with the passed name
+ * @param name name to be greeted
+ * @returns a greetings
+ */
+export function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
---
+++ packages/migpalg-pack/lib/index.ts
@@ -0,0 +1,3 @@
+export function pack(): number {
+  return 0;
+}
