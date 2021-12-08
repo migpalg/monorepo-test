c783f9ce6ab46c65269909087d6eff7524dbe5d0
Miguel González <migpalg@gmail.com>
2021-12-07 20:08:25 -0500

    feat: added rollup.js to the project
    We will use this automation tool due its simplocity and power

--- packages/migpalg-pack/lib/index.ts
+++ packages/migpalg-pack/lib/index.ts
@@ -1,3 +1,6 @@
+import { greet } from "@migpalg/core";
+
 export function pack(): number {
+  console.log(greet("Luis"));
   return 0;
 }
---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+// @packages
+import { defineConfig } from "rollup";
+
+/**
+ * Base configuration to bundle any package listed in the worspaces
+ */
+export default defineConfig({});
