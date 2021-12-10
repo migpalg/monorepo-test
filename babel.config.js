8088e62143ccd4e1b2dc2feb5cc3e326e8236433
Miguel González <migpalg@gmail.com>
2021-12-09 20:11:42 -0500

    feat: added babel

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  comments: false,
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "defaults",
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
